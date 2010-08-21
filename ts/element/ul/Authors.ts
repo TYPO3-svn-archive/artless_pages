e8c5206f53198117dc781ebfa85c3e0b1b8e24b2
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-21 13:14:59 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless_pages/trunk@37085 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/element/ul/Authors.ts
@@ -0,0 +1,19 @@
+A.element.ul.Authors = HMENU
+A.element.ul.Authors {
+	
+	special = list
+	special.value.field = tx_artlesspages_authors
+	
+	1 = TMENU
+	1 {
+		
+		wrap = <ul class="A_authors">|</ul>
+		
+		NO = 1
+		NO {
+			allWrap = <li>|</li>
+		}
+		
+	}
+	
+}
---
+++ ts/element/ul/Categories.ts
@@ -0,0 +1,19 @@
+A.element.ul.Categories = HMENU
+A.element.ul.Categories {
+	
+	special = list
+	special.value.field = tx_artlesspages_categories
+	
+	1 = TMENU
+	1 {
+		
+		wrap = <ul class="A_categories">|</ul>
+		
+		NO = 1
+		NO {
+			allWrap = <li>|</li>
+		}
+		
+	}
+	
+}
