4cb713a96b9e567c4e34fb6a6149c850de3a644b
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-26 15:44:13 +0000

    Added A.element.ul.RelatedPagesMenu
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless_pages/trunk@37290 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/element/ul/RelatedPagesMenu.ts
@@ -0,0 +1,9 @@
+A.element.ul.RelatedPagesMenu =< A.element.ul.Menu
+A.element.ul.RelatedPagesMenu {
+	
+	wrap = <div class="A_relatedpagesmenu A_menu">|</div>
+	
+	special = directory
+	special.value = {$A.pids.relatedPagesMenu}
+	
+}
