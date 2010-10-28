1fa7e0a59afcfc0144bf33710cac7c3ab75ec765
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-10-28 18:07:26 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless_pages/trunk@39629 735d13b6-9817-0410-8766-e36946ffe9aa

--- ts/model/Page.ts
+++ ts/model/Page.ts
@@ -1,55 +1,47 @@
 A.model.Page {
 	
-	p {
-		
-		authors =< A.element.span.RelatedPages
-		authors {
-			stdWrap.outerWrap = <p class="A_authors">|</p>
-			special.value.field = tx_artlesspages_authors
-			stdWrap.fieldRequired = tx_artlesspages_authors
-			stdWrap.prepend = TEXT
-			stdWrap.prepend {
-				data = LLL:EXT:artless_pages/locallang_db.xml:pages.tx_artlesspages_authors
-				wrap = <span class="A_prefix">|</span>
-			}
+	authors =< A.element.span.RelatedPages
+	authors {
+		stdWrap.outerWrap = <p class="A_authors">|</p>
+		special.value.field = tx_artlesspages_authors
+		stdWrap.fieldRequired = tx_artlesspages_authors
+		stdWrap.prepend = TEXT
+		stdWrap.prepend {
+			data = LLL:EXT:artless_pages/locallang_db.xml:pages.tx_artlesspages_authors
+			wrap = <span class="A_prefix">|</span>
 		}
-		
-		categories =< A.element.span.RelatedPages
-		categories {
-			wrap = <p class="A_categories">|</p>
-			special.value.field = tx_artlesspages_categories
-			stdWrap.fieldRequired = tx_artlesspages_categories
-		}
-		
-		links =< A.element.span.RelatedPages
-		links {
-			wrap = <p class="A_links">|</p>
-			special.value.field = tx_artlesspages_links
-			stdWrap.fieldRequired = tx_artlesspages_links
-		}
-		
 	}
 	
-	ul {
-		
-		authors =< A.element.ul.RelatedPages
-		authors {
-			special.value.field = tx_artlesspages_authors
-			stdWrap.fieldRequired = tx_artlesspages_authors
-		}
-		
-		categories =< A.element.ul.RelatedPages
-		categories {
-			special.value.field = tx_artlesspages_categories
-			stdWrap.fieldRequired = tx_artlesspages_categories
-		}
-		
-		links =< A.element.ul.RelatedPages
-		links {
-			special.value.field = tx_artlesspages_links
-			stdWrap.fieldRequired = tx_artlesspages_links
-		} 
-		
+	categories =< A.element.span.RelatedPages
+	categories {
+		wrap = <p class="A_categories">|</p>
+		special.value.field = tx_artlesspages_categories
+		stdWrap.fieldRequired = tx_artlesspages_categories
+	}
+	
+	links =< A.element.span.RelatedPages
+	links {
+		wrap = <p class="A_links">|</p>
+		special.value.field = tx_artlesspages_links
+		stdWrap.fieldRequired = tx_artlesspages_links
+	}
+	
+	authors =< A.element.ul.RelatedPages
+	authors {
+		special.value.field = tx_artlesspages_authors
+		stdWrap.fieldRequired = tx_artlesspages_authors
+	}
+	
+	categories =< A.element.ul.RelatedPages
+	categories {
+		special.value.field = tx_artlesspages_categories
+		stdWrap.fieldRequired = tx_artlesspages_categories
+	}
+	
+	links =< A.element.ul.RelatedPages
+	links {
+		special.value.field = tx_artlesspages_links
+		stdWrap.fieldRequired = tx_artlesspages_links
 	}
 	
 	datePeriod = COA
